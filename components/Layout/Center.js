8f2a281fda954186e94bb57999f4976a37bd00da
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-05-19 17:49:11 +0545

    Update Center.js

--- components/Layout/Center.js
+++ components/Layout/Center.js
@@ -1,16 +1,18 @@
 import { styled } from "styled-components";
 
 const StyledDiv = styled.div`
-max-width: 1350px;
-margin: 0 40px;
+  max-width: 1350px;
+  margin: 0 40px;
 
+  @media (max-width: 768px) {
+    margin: 0 20px; /* Reduce margin for mobile view */
+  }
 
+  @media (max-width: 480px) {
+    margin: 0 10px; /* Further reduce margin for smaller screens */
+  }
 `;
 
-export default function Center({children}){
-    return (
-<StyledDiv>
-    {children}
-</StyledDiv>
-    );
+export default function Center({ children }) {
+  return <StyledDiv>{children}</StyledDiv>;
 }
