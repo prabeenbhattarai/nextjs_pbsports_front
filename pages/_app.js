1f17766a627073a4749cb4b5c04236818243f924
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-05-22 16:24:50 +0545

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import NextNProgress from 'nextjs-progressbar';
-import { SessionProvider } from "next-auth/react"; // Import SessionProvider from NextAuth
+import { SessionProvider } from "next-auth/react";
+import GoogleSignIn from '@/components/GoogleSignIn'; // Import the GoogleSignIn component
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,900;1,800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
@@ -15,11 +16,12 @@ const GlobalStyles = createGlobalStyle`
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}> {/* Wrap your component tree with SessionProvider */}
+    <SessionProvider session={pageProps.session}>
       <>
         <GlobalStyles />
         <NextNProgress color="#1AA211" startPosition={0.3} stopDelayMs={200} height={5} showOnShallow={true} />
         <Component {...pageProps} />
+        <GoogleSignIn /> {/* Add the GoogleSignIn component */}
       </>
     </SessionProvider>
   );
