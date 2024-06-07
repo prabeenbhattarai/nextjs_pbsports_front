7f98edb638c95d144ce38a0af46580a0f32e95d2
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-07 08:43:45 +0545

    Update Featured.js

--- components/Layout/Featured.js
+++ components/Layout/Featured.js
@@ -1,7 +1,6 @@
+import { useState, useEffect } from "react";
 import { styled } from "styled-components";
 import Center from "./Center";
-import PrimaryBtn from "../Button/Button";
-import Button from "../Button/Button";
 import ButtonLink from "../Button/ButtonLink";
 import Image from "next/image";
 
@@ -9,8 +8,8 @@ const Bg = styled.div`
   background-color: #000;
   color: #000;
   padding: 50px 0;
-  border-bottom-left-radius: 20px; /* Add border radius to bottom-left corner */
-  border-bottom-right-radius: 20px; 
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
 `;
 
 const Title = styled.h1`
@@ -18,7 +17,6 @@ const Title = styled.h1`
   font-weight: 100%;
   font-size: 2.5rem;
   font-style: oblique;
-
   color: #fff;
 `;
 
@@ -26,7 +24,7 @@ const Titleh1 = styled.h1`
   margin: 0;
   font-weight: 100%;
   font-size: 1rem;
-  color: #EC250E;
+  color: #ec250e;
   align-items: center;
 `;
 
@@ -36,14 +34,13 @@ const Desc = styled.p`
 `;
 
 const DescTime = styled.p`
-  color: #D9D9D9;
+  color: #d9d9d9;
   font-size: 1rem;
 `;
 
 const Column = styled.div`
   display: flex;
   align-items: center;
-  
 `;
 
 const ColumnWrapper = styled.div`
@@ -52,10 +49,10 @@ const ColumnWrapper = styled.div`
   gap: 40px;
   img {
     max-width: 100%;
-    height:auto;
+    height: auto;
     border-radius: 25px;
   }
-  @media screen and (min-width: 768px){
+  @media screen and (min-width: 768px) {
     grid-template-columns: 1.1fr 0.9fr;
   }
 `;
@@ -65,12 +62,58 @@ const ButtonWrapper = styled.div`
   gap: 10px;
 `;
 
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  img {
+    transition: opacity 1s ease-in-out;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: auto;
+    opacity: 0;
+  }
+  img.active {
+    opacity: 1;
+  }
+`;
+
 export default function Featured({ schedule }) {
+  const [images, setImages] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    async function fetchImages() {
+      const response = await fetch("/api/images");
+      const data = await response.json();
+      setImages(data);
+    }
+    fetchImages();
+  }, []);
+
+  useEffect(() => {
+    if (images.length > 0) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 5000); // Change image every 5 seconds
+      return () => clearInterval(interval);
+    }
+  }, [images.length]);
+
   // Function to convert time string to formatted time
   const formatTime = (timeString) => {
     const date = new Date(timeString);
-    const options = { weekday: 'long', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
-    return date.toLocaleDateString('en-US', options);
+    const options = {
+      weekday: "long",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    };
+    return date.toLocaleDateString("en-US", options);
   };
 
   return (
@@ -84,10 +127,20 @@ export default function Featured({ schedule }) {
               <Desc>{schedule.description}</Desc>
               <DescTime>{formatTime(schedule.time)} NPT</DescTime>
               <ButtonWrapper>
-                
-                <ButtonLink href={'/Games/schedule/' + schedule._id} primary size="l">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z" />
+                <ButtonLink href={"/Games/schedule/" + schedule._id} primary size="l">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
+                    />
                   </svg>
                   Watch Now
                 </ButtonLink>
@@ -95,7 +148,18 @@ export default function Featured({ schedule }) {
             </div>
           </Column>
           <Column>
-            <Image src="/bn1.png" alt="" width={1000} height={700} />
+            <ImageWrapper>
+              {images.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image.url}
+                  alt=""
+                  width={1000}
+                  height={700}
+                  className={index === currentIndex ? "active" : ""}
+                />
+              ))}
+            </ImageWrapper>
           </Column>
         </ColumnWrapper>
       </Center>
