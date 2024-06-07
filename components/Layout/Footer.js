6946509ff6ad3b24d93335315ec2f4f4fe87d002
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-07 17:12:52 +0545

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -6,6 +6,10 @@ const FooterWrapper = styled.footer`
   background-color: #000; /* Black background color */
   color: #fff; /* White text color */
   padding: 20px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
 `;
 
 const Container = styled.div`
@@ -53,7 +57,6 @@ const Copyright = styled.div`
   margin-top: 20px; /* Adjust spacing from the links */
   font-size: 0.8rem;
   color: #aaa;
-
 `;
 const Text = styled.div`
   text-align: center;
@@ -65,19 +68,20 @@ const Text = styled.div`
 
 export default function Footer() {
   return (
- 
     <FooterWrapper>
-      <Container>
-        <div>
-          <Logo src="/logo.png" alt="Logo" /> {/* Replace with your logo image */}
+      <Center>
+        <Container>
+          <div>
+            <Logo src="/logo.png" alt="Logo" /> {/* Replace with your logo image */}
           </div>
-          <Text>The live stream content provided on this website is sourced from third-party platforms. We are not the original event broadcaster and utilize these platforms to fetch live streams for our users. All rights and copyrights for the content belong to the respective broadcasters. We do not claim ownership of the content streamed through our platform. If you have any concerns regarding the content, please contact the original broadcaster directly.
-      </Text> 
-      </Container>
-      <Copyright>
-        &copy; {new Date().getFullYear()} PbSports | Live Anytime, Anywhere | All Right Reserved. {/* Update with your company name */}
-      </Copyright>
+          <Text>
+            The live stream content provided on this website is sourced from third-party platforms. We are not the original event broadcaster and utilize these platforms to fetch live streams for our users. All rights and copyrights for the content belong to the respective broadcasters. We do not claim ownership of the content streamed through our platform. If you have any concerns regarding the content, please contact the original broadcaster directly.
+          </Text>
+        </Container>
+        <Copyright>
+          &copy; {new Date().getFullYear()} PbSports | Live Anytime, Anywhere | All Right Reserved. {/* Update with your company name */}
+        </Copyright>
+      </Center>
     </FooterWrapper>
- 
   );
 }
