4cf3f5bb4660aa1a4b8f587429e4b694c6e7b1ca
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-07 13:25:00 +0545

    Update Featured.js

--- components/Layout/Featured.js
+++ components/Layout/Featured.js
@@ -1,57 +1,99 @@
-import { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Center from './Center';
-import Image from 'next/image';
+import { styled } from "styled-components";
+import Center from "./Center";
+import ButtonLink from "../Button/ButtonLink";
+import Image from "next/image";
 
 const Bg = styled.div`
   background-color: #000;
   color: #000;
-  padding: 50px 0;
-  border-bottom-left-radius: 20px;
-  border-bottom-right-radius: 20px;
-`;
-
-const ColumnWrapper = styled.div`
   position: relative;
-  display: grid;
-  grid-template-columns: 1fr;
+  padding: 10px 0;
+  border-bottom-left-radius: 20px; /* Add border radius to bottom-left corner */
+  border-bottom-right-radius: 20px; 
 `;
 
-const FadeOffImage = styled(Image)`
+const Title = styled.h1`
+  margin: 0;
+  font-weight: 100%;
+  font-size: 2.5rem;
+  font-style: oblique;
+  color: #fff;
+`;
+
+const Titleh1 = styled.h1`
+  margin: 0;
+  font-weight: 100%;
+  font-size: 1rem;
+  color: #EC250E;
+  align-items: center;
+`;
+
+const Desc = styled.p`
+  color: #aaa;
+  font-size: 1rem;
+`;
+
+const DescTime = styled.p`
+  color: #D9D9D9;
+  font-size: 1rem;
+`;
+
+const ContentWrapper = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  transition: opacity 0.5s ease-in-out;
-  opacity: ${({ active }) => (active ? 1 : 0)};
+  top: 50%;
+  left: 5%;
+  transform: translateY(-50%);
+  max-width: 50%;
+  color: #fff;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 500px; /* Adjust the height as needed */
+  overflow: hidden;
+  border-radius: 25px;
+  position: relative;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 10px;
 `;
 
 export default function Featured({ schedule }) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [images, setImages] = useState(['/image3.png', '/image2.png']);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Change image every 5 seconds
-
-    return () => clearInterval(intervalId);
-  }, [images.length]);
+  // Function to convert time string to formatted time
+  const formatTime = (timeString) => {
+    const date = new Date(timeString);
+    const options = { weekday: 'long', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+    return date.toLocaleDateString('en-US', options);
+  };
 
   return (
     <Bg>
       <Center>
-        <ColumnWrapper>
-          {images.map((src, index) => (
-            <FadeOffImage
-              key={src}
-              src={src}
-              alt=""
-              width={1000}
-              height={700}
-              active={index === activeIndex}
-            />
-          ))}
-        </ColumnWrapper>
+        <ImageWrapper>
+          <Image src="/image3.png" alt="" layout="fill" objectFit="cover" />
+          <ContentWrapper>
+            <Titleh1>Recently scheduled</Titleh1>
+            <Title>{schedule.title}</Title>
+            <Desc>{schedule.description}</Desc>
+            <DescTime>{formatTime(schedule.time)}</DescTime>
+            <ButtonWrapper>
+              <ButtonLink href={'/Games/schedule/' + schedule._id} primary size="l">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z" />
+                </svg>
+                Watch Now
+              </ButtonLink>
+            </ButtonWrapper>
+          </ContentWrapper>
+        </ImageWrapper>
       </Center>
     </Bg>
   );
