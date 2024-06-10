5aa1d2cc80972a0b8b68ba2b09974564988cc6b1
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-10 21:54:07 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import Live from "@/components/Live/Live";
 
-
 import { mongooseConnect } from "@/lib/mongoose";
 import { Highlight } from "@/models/Highlight";
 import { Schedule } from "@/models/Schedule";
@@ -25,79 +24,93 @@ import IccTestLive from "@/components/Live/cricket/ICC_Test/IccTestLive";
 import AsiaLive from "@/components/Live/cricket/Asia_Cup/AsiaLive";
 import IccT20Live from "@/components/Live/cricket/ICC_T20/IccT20Live";
 
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px', // Adjust gap between components
+  },
+};
 
-
-
-
-
-
-
-
-
-
-
-export default function HomePage({featuredSchedule,liveSchedule,crickethighlight, footballSchedule, footballhighlight, ufchighlight,national,formula,copa,epl,uefa,euro,fifa,iccwc,icctest,asiacup,icct20}) {
- 
-   return(
-
-    <div> 
+export default function HomePage({
+  featuredSchedule,
+  liveSchedule,
+  crickethighlight,
+  footballSchedule,
+  footballhighlight,
+  ufchighlight,
+  national,
+  formula,
+  copa,
+  epl,
+  uefa,
+  euro,
+  fifa,
+  iccwc,
+  icctest,
+  asiacup,
+  icct20,
+}) {
+  return (
+    <div style={styles.container}>
       <Header />
-      <Featured  schedule={featuredSchedule}/> 
-      <Live schedule={liveSchedule} />
-    <IccT20Live icct20={icct20}/>
-  
-    <AsiaLive asiacup={asiacup}/>
-      <IccLive iccwc={iccwc}/>
-    <IccTestLive icctest={icctest}/>
-      <FootballLive football={footballSchedule}/> 
-    <FifaLive fifa={fifa}/>
-   <CopaLive copa={copa}/>
-    <EuroLive euro={euro}/>
-   <EplLive epl={epl}/>
-    <UefaLive uefa={uefa}/>
-     <NationalLive national={national}/> 
-   <FormulaLive formula={formula}/> 
-
-<CricketHighlights crickethighlight ={crickethighlight}/> 
-      <FootballHighlights footballhighlight={footballhighlight} />
-      <UfcHighlights ufchighlight ={ufchighlight} />
-
+      <Featured schedule={featuredSchedule} />
+      {liveSchedule.length > 0 && <Live schedule={liveSchedule} />}
+      {icct20.length > 0 && <IccT20Live icct20={icct20} />}
+      {asiacup.length > 0 && <AsiaLive asiacup={asiacup} />}
+      {iccwc.length > 0 && <IccLive iccwc={iccwc} />}
+      {icctest.length > 0 && <IccTestLive icctest={icctest} />}
+      {footballSchedule.length > 0 && <FootballLive football={footballSchedule} />}
+      {fifa.length > 0 && <FifaLive fifa={fifa} />}
+      {copa.length > 0 && <CopaLive copa={copa} />}
+      {euro.length > 0 && <EuroLive euro={euro} />}
+      {epl.length > 0 && <EplLive epl={epl} />}
+      {uefa.length > 0 && <UefaLive uefa={uefa} />}
+      {national.length > 0 && <NationalLive national={national} />}
+      {formula.length > 0 && <FormulaLive formula={formula} />}
+      {crickethighlight.length > 0 && <CricketHighlights crickethighlight={crickethighlight} />}
+      {footballhighlight.length > 0 && <FootballHighlights footballhighlight={footballhighlight} />}
+      {ufchighlight.length > 0 && <UfcHighlights ufchighlight={ufchighlight} />}
       <Footer />
     </div>
   );
 }
 
-export async function getServerSideProps(){
-  
+export async function getServerSideProps() {
   await mongooseConnect();
-  const featuredSchedule = await Schedule.findOne({}, null, { sort: { 'createdAt': -1 } });
-  const cricketCategory = await Category.findOne({ name: 'Cricket' });
-  const footballCategory = await Category.findOne({ name: 'Football' });
-  const ufcCategory = await Category.findOne({ name: 'UFC' });
-   const nationalCategory = await Category.findOne({ name: 'NationalGames' });
-    const formulaCategory = await Category.findOne({ name: 'Formula1' });
-     const copaCategory = await Category.findOne({ name: 'CopaAmerica' });
-     const eplCategory = await Category.findOne({ name: 'English_Premier_League' });
-      const uefaCategory = await Category.findOne({ name: 'UEFA_Champions_League' });
-       const fifaCategory = await Category.findOne({ name: 'FIFA_Worldcup' });
-        const iccwcCategory = await Category.findOne({ name: 'ICC_Cricket_Worldcup' });
-         const icctestCategory = await Category.findOne({ name: 'ICC_World_Test_Championship' });
-          const asiacupCategory = await Category.findOne({ name: 'Asia_Cup' });
-           const icct20Category = await Category.findOne({ name: 'ICC_T20_Worldcup' });
-            const euroCategory = await Category.findOne({ name: 'Euro_Cup' });
 
+  let featuredSchedule = null;
+  let cricketCategory = null;
+  let footballCategory = null;
+  let ufcCategory = null;
+  let nationalCategory = null;
+  let formulaCategory = null;
+  let copaCategory = null;
+  let eplCategory = null;
+  let uefaCategory = null;
+  let fifaCategory = null;
+  let iccwcCategory = null;
+  let icctestCategory = null;
+  let asiacupCategory = null;
+  let icct20Category = null;
+  let euroCategory = null;
 
-
-
-
-
-
-
-
-    
-
-
-
+  try {
+    featuredSchedule = await Schedule.findOne({}, null, { sort: { createdAt: -1 } });
+    cricketCategory = await Category.findOne({ name: 'Cricket' });
+    footballCategory = await Category.findOne({ name: 'Football' });
+    ufcCategory = await Category.findOne({ name: 'UFC' });
+    nationalCategory = await Category.findOne({ name: 'NationalGames' });
+    formulaCategory = await Category.findOne({ name: 'Formula1' });
+    copaCategory = await Category.findOne({ name: 'CopaAmerica' });
+    eplCategory = await Category.findOne({ name: 'English_Premier_League' });
+    uefaCategory = await Category.findOne({ name: 'UEFA_Champions_League' });
+    fifaCategory = await Category.findOne({ name: 'FIFA_Worldcup' });
+    iccwcCategory = await Category.findOne({ name: 'ICC_Cricket_Worldcup' });
+    icctestCategory = await Category.findOne({ name: 'ICC_World_Test_Championship' });
+    asiacupCategory = await Category.findOne({ name: 'Asia_Cup' });
+    icct20Category = await Category.findOne({ name: 'ICC_T20_Worldcup' });
+    euroCategory = await Category.findOne({ name: 'Euro_Cup' });
 
 
 
