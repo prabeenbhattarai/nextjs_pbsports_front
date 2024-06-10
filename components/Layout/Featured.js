4074c2372a06fbe66f8a38e9f4d6baeb0bc98b1e
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-10 22:24:06 +0545

    Update Featured.js

--- components/Layout/Featured.js
+++ components/Layout/Featured.js
@@ -93,7 +93,7 @@ const ContentWrapper = styled.div`
 
 export default function Featured({ schedule }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const images = [ "/image3.png", "/image8.jpg"];
+  const images = [ "/image3.png", "/image8.jpg", "/image9.png"];
 
   useEffect(() => {
     const interval = setInterval(() => {
