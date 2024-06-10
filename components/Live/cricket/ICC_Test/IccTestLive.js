3d273d41569bcca21bc948b55b9c4ad003e48d7e
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-10 18:06:27 +0545

    Update IccTestLive.js

--- components/Live/cricket/ICC_Test/IccTestLive.js
+++ components/Live/cricket/ICC_Test/IccTestLive.js
@@ -4,7 +4,7 @@ import { useState, useRef, useEffect } from "react";
 import { styled } from "styled-components";
 import LiveBox from "../../../LiveBox";
 import Center from "../../../Layout/Center";
-import IccTestBox from "./IccTestbox";
+import IccTestBox from "./IccTestBox";
 
 const ScheduleGridContainer = styled.div`
   overflow-x: hidden;
