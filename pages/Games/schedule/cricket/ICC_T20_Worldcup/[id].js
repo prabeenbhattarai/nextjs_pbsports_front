7f7fd7c810efbfc16cf91e7dd1804fafa2007b82
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-11 11:10:42 +0545

    Update [id].js

--- pages/Games/schedule/cricket/ICC_T20_Worldcup/[id].js
+++ pages/Games/schedule/cricket/ICC_T20_Worldcup/[id].js
@@ -5,48 +5,54 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Schedule } from "@/models/Schedule";
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
-import { useSession } from "next-auth/react";
+import Live from "@/components/Live/Live";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 import Link from "next/link";
-import { useRouter } from 'next/router';
+
+
+
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  align-items: flex-start;
-  padding-top: 5px;
-  background-color: #000;
+  align-items: flex-start; /* Align items from the top */
+  padding-top: 5px; /* Minimum gap from the top */
+  background-color:#000;
   padding-bottom: 100px;
 
-  @media (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
-  }
+
 `;
 
 const VideoContainer = styled.div`
-  width: 100%;
-  position: relative;
+width: 100%;
+position: relative;
 `;
 
 const VideoFrame = styled.iframe`
-  height: 100vh !important;
-  padding: 0;
-  margin: 0;
   width: 100%;
+  height: 580px;
+
+`;
+
+const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 70px 20px;
 `;
 
 const RightSide = styled.div`
-  width: 100%;
-  height: 100vh;
+  width: 50%;
   position: relative;
 `;
 
 const Image = styled.img`
   width: 100%;
-  height: 100vh;
-  object-fit: cover;
+  height: auto;
+  opacity: 0.5; /* Adjust image opacity */
   border-radius: 20px;
-  opacity: 0.5;
 `;
 
 const Overlay = styled.div`
@@ -55,62 +61,65 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  padding: 20px;
-  box-sizing: border-box;
+  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent overlay */
+`;
 
-  @media (max-width: 768px) {
-    padding: 10px;
-  }
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+`;
+const DescContainer = styled.div`
+  display: flex;
+  align-items: center;
+  overflow: hidden; /* Hide overflowing content */
+  text-overflow: ellipsis; /* Add ellipsis for text overflow */
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* Limit to 2 lines */
+  -webkit-box-orient: vertical;
 `;
 
 const WatchingText = styled.h2`
-  font-size: 0.9rem;
-  color: #fff;
-  margin: 10px 10px 10px 0;
+  font-size: .9rem;
+  color: #fff; /* Grey color for "Now Watching" section */
+  margin: 10px 10px 10px 0; 
 `;
+
 
 const Title = styled.h1`
-  font-size: 1.2rem;
-  color: #fe7706;
+  font-size: .9rem;
+  color: #FE7706; /* Title color as mentioned */
   margin: 10px 0;
-  text-transform: uppercase;
+  text-transform: uppercase; 
 
-  @media (max-width: 768px) {
-    font-size: 1rem;
-  }
 `;
-
-const Desc = styled.p`
-  font-size: 0.8rem;
+const Desc = styled.h1`
+  font-size: 0.7rem;
   color: #fff;
   margin: 10px 0;
-  font-family: 'Arial', sans-serif;
-  overflow: hidden;
-  text-overflow: ellipsis;
+  font-family: 'Arial', sans-serif; /* Change font family */
+  overflow: hidden; /* Hide overflowing content */
+  text-overflow: ellipsis; /* Add ellipsis for text overflow */
   display: -webkit-box;
-  -webkit-line-clamp: 2;
+  -webkit-line-clamp: 2; /* Limit to 2 lines */
   -webkit-box-orient: vertical;
+`;
 
-  @media (max-width: 768px) {
-    font-size: 0.7rem;
-  }
+const Description = styled.p`
+  margin: 10px 0;
+  text-align: center;
+  font-size: 1.2rem; /* Adjust font size */
+  font-family: 'Arial', sans-serif; /* Change font family */
+  color:#aaa;
+  
 `;
 
 const TimerContainer = styled.div`
   margin: 10px 0;
-  color: #dc5b0d;
+  color: #DC5B0D;
   font-size: 1.5rem;
   text-align: center;
-  font-family: 'Arial', sans-serif;
-
-  @media (max-width: 768px) {
-    font-size: 1.2rem;
-  }
+  font-family: 'Arial', sans-serif; /* Change font family */
 `;
 
 const ButtonContainer = styled.div`
@@ -122,23 +131,28 @@ const ButtonContainer = styled.div`
 
 const LoginButton = styled.button`
   padding: 10px 20px;
-  background-color: #dc5b0d;
+  background-color: #DC5B0D;
   color: #ffffff;
   border: none;
   border-radius: 5px;
   cursor: pointer;
   margin-top: 10px;
+
 `;
 
 const AlreadyUserText = styled.p`
-  color: #dc5b0d;
+  color: #DC5B0D;
+`;
+const VideoFrameWrapper = styled.div`
+
 `;
 
-export default function SchedulePage({ schedule }) {
+export default function SchedulePage({ schedule,liveSchedule}) {
   const { data: session } = useSession();
+ 
+
   const [showVideo, setShowVideo] = useState(false);
   const [timeLeft, setTimeLeft] = useState(0);
-  const router = useRouter();
 
   useEffect(() => {
     const scheduledTime = new Date(schedule.time).getTime();
@@ -163,64 +177,60 @@ export default function SchedulePage({ schedule }) {
       return () => clearInterval(timerInterval);
     }
   }, [schedule]);
-
-  useEffect(() => {
-    if (showVideo && !session) {
-      router.push(`/user/Auth/login?callbackUrl=${encodeURIComponent(router.asPath)}`);
-    }
-  }, [showVideo, session, router]);
+  
+  
 
   return (
     <>
       <Header />
-      <Container>
-        {showVideo ? (
-          session ? (
+        <Container>
+          {showVideo ? (
             <VideoContainer>
+            <VideoFrameWrapper>
               <VideoFrame
                 src={schedule.url}
-                allowFullScreen
-                allowTransparency="true"
-                frameBorder="0"
+                frameborder="0"
+                allowfullscreen
               ></VideoFrame>
-              <Center>
-                <WatchingText>Now Watching:</WatchingText>
-                <Title>{schedule.title}</Title>
-                <Desc>{schedule.description}</Desc>
-              </Center>
-            </VideoContainer>
+            </VideoFrameWrapper>
+            <Center>
+            <TitleContainer>
+              
+  <WatchingText>Now Watching:</WatchingText>
+  <Title>{schedule.title}</Title>
+</TitleContainer>
+
+<DescContainer>
+  <Desc>{schedule.description}</Desc>
+</DescContainer>
+</Center>
+          </VideoContainer>
+
           ) : (
-            <RightSide>
-<Image src={schedule && schedule.images ? schedule.images[0] : ''} alt="Scheduled Event" />
-              <Overlay>
-                <Title>Please log in to watch the video</Title>
-                <Link href={`/user/Auth/login?callbackUrl=${encodeURIComponent(router.asPath)}`}>
-                  <LoginButton>Login</LoginButton>
-                </Link>
-              </Overlay>
-            </RightSide>
-          )
-        ) : (
-          <RightSide>
-            <Image src={schedule.images[0]} alt="Scheduled Event" />
-            <Overlay>
-              <TimerContainer>
-                {timeLeft > 0 ? `${formatTimeLeft(timeLeft)}` : ''}
-              </TimerContainer>
-              <Title>{schedule.title}</Title>
-              <Desc>{schedule.description}</Desc>
-              {!session && (
-                <ButtonContainer>
-                  <Link href={`/user/Auth/login?callbackUrl=${encodeURIComponent(router.asPath)}`}>
-                    <LoginButton>Login</LoginButton>
-                  </Link>
-                  <AlreadyUserText>Already a user?</AlreadyUserText>
-                </ButtonContainer>
-              )}
-            </Overlay>
-          </RightSide>
-        )}
-      </Container>
+            <Container>
+              <LeftSide>
+                <TimerContainer>
+                  {timeLeft > 0 ? ` starts in: ${formatTimeLeft(timeLeft)}` : ''}
+                </TimerContainer>
+                <Title>{schedule.title}</Title>
+                <Description>{schedule.description}</Description>
+                {!session && (
+  <ButtonContainer>
+     <Link href="/user/Auth/login">
+    <LoginButton >Login</LoginButton></Link>
+    <AlreadyUserText>Already a user?</AlreadyUserText>
+  </ButtonContainer>
+)}
+              </LeftSide>
+              <RightSide>
+                <Overlay />
+                <Image src={schedule.images[0]} alt="Scheduled Event" />
+              </RightSide>
+            </Container>
+          )}
+        </Container>
+       
+
       <Footer />
     </>
   );
@@ -228,9 +238,23 @@ export default function SchedulePage({ schedule }) {
 
 function formatTimeLeft(timeLeft) {
   const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+
   const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-  return `${days}D ${hours}H :${minutes}M :${seconds}s`;
+  return ` ${days}D ${hours}H :${minutes}M :${seconds}s`;
 }
+
+export async function getServerSideProps(context) {
+  await mongooseConnect();
+  const { id } = context.query;
+  const schedule = await Schedule.findById(id);
+  const liveSchedule = await Schedule.find({}, null, {sort: {'_id':-1}, limit:10});
+  return {
+    props: {
+      schedule: JSON.parse(JSON.stringify(schedule)),
+      liveSchedule: JSON.parse(JSON.stringify(liveSchedule))},
+
+    }
+  };
